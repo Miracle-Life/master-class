f0eb85adea2aa5b58db48942a2382e01a971262e
belov <belovdenis777@gmail.com>
2021-06-14 01:07:46 +0300

    Удаление фильмов

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import {moviesData} from "../moviesData"
+import MovieItem from "./MovieItem";
 
-function App() {
-  return <div>Hello React!</div>;
-}
 
-export default App;
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: moviesData,
+
+        }
+        //если у нас  removeMovies (movie) {} то используем
+        // this.removeMovies=this.removeMovies.bind(this);
+        //но мы применяем стрелочную функцию
+    }
+
+    removeMovies = (movie) => {
+        const updateMovies = this.state.movies.filter((item) => {
+                return item.id !== movie.id
+            }
+        )
+        this.setState({
+            movies: updateMovies
+        });
+    }
+
+    render() {
+        return (<div>
+            {this.state.movies.map(movie => {
+                return <MovieItem
+                    key={movie.id}
+                    movie={movie}
+                    removeMovies={this.removeMovies}
+                />
+            })}
+        </div>)
+    }
+
+
+};
+
---
+++ src/components/MovieItem.jsx
@@ -0,0 +1,1 @@
+import React from "react";const MovieItem = props => {    return (        <div>            <p>{props.movie.title}</p>            <button onClick={props.removeMovies.bind(this, props.movie)}>                Delete movie            </button>        </div>    )}export default MovieItem;
