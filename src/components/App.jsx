bf11c3b0fe532e2110f26e6dfb3e41352f459976
belov <belovdenis777@gmail.com>
2021-06-19 13:34:07 +0300

    Обновляем список фильмов в зависимости от выбранной сортировки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import {API_KEY_3, API_URL} from "../utils/api";
 import MoviesTabs from "./MoviesTabs";
 
 
-
 export default class App extends React.Component {
     constructor(props) {
         super(props)
@@ -17,13 +16,25 @@ export default class App extends React.Component {
         //если у нас  removeMovies (movie) {} то используем
         // this.removeMovies=this.removeMovies.bind(this);
         //но мы применяем стрелочную функцию
-
         console.log('constructor')
-
     }
 
-    componentDidMount(promise) {
-        const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`
+    componentDidMount() {
+        this.getMovies();
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('didUpdate');
+        console.log('prevProps', prevProps, prevState);
+        console.log('this', this.props, this.state);
+        if (prevState.sort_by !== this.state.sort_by) {
+            console.log('api');
+            this.getMovies();
+        }
+    }
+
+    getMovies = () => {
+        const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`;
         fetch(link)
             .then(r => r.json())
             .then((data) => {
@@ -32,10 +43,6 @@ export default class App extends React.Component {
                     movies: data.results
                 })
             })
-    };
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log('didUpdate');
     }
 
     removeMovies = (movie) => {
@@ -59,7 +66,6 @@ export default class App extends React.Component {
         })
     }
 
-
     addMovieToWillWatch = (movie) => {
         // console.log(movie)
         const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
@@ -78,49 +84,48 @@ export default class App extends React.Component {
 
     render() {
         console.log('render')
-        return (<div className='container'>
-            <div className="row">
-                <div className="col-9">
-                    <div className="row mb-4">
-                        <div className="row">
-                            <div className="col-12">
-                                <MoviesTabs
-                                    sort_by={this.state.sort_by}
-                                    updateSortBy={this.updateSortBy}
-                                />
+        return (
+            <div className='container'>
+                <div className="row">
+                    <div className="col-9">
+                        <div className="row mb-4">
+                            <div className="row">
+                                <div className="col-12">
+                                    <MoviesTabs
+                                        sort_by={this.state.sort_by}
+                                        updateSortBy={this.updateSortBy}
+                                    />
+                                </div>
                             </div>
-                        </div>
 
-                        {this.state.movies.map(movie => {
-                            return (<div className='col-6 mb-4' key={movie.id}>
-                                <MovieItem
-                                    movie={movie}
-                                    removeMovies={this.removeMovies}
-                                    removeMoviesFromWillWatch={this.removeMoviesFromWillWatch}
-                                    addMovieToWillWatch={this.addMovieToWillWatch}
-                                />
-                            </div>)
-                        })}
+                            {this.state.movies.map(movie => {
+                                return (<div className='col-6 mb-4' key={movie.id}>
+                                    <MovieItem
+                                        data={movie}
+                                        removeMovies={this.removeMovies}
+                                        removeMoviesFromWillWatch={this.removeMoviesFromWillWatch}
+                                        addMovieToWillWatch={this.addMovieToWillWatch}
+                                    />
+                                </div>)
+                            })}
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <h4>Will Watch: {this.state.moviesWillWatch.length} movies</h4>
+                        <ul className="list-group">
+                            {this.state.moviesWillWatch.map(movie => (
+                                <li key={movie.id} className="list-group-item">
+                                    <div className="d-flex justify-content-between">
+                                        <p>{movie.title}</p>
+                                        <p>{movie.vote_average}</p>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
-                <div className="col-3">
-                    <h4>Will Watch: {this.state.moviesWillWatch.length} movies</h4>
-                    <ul className="list-group">
-                        {this.state.moviesWillWatch.map(movie => (
-                            <li key={movie.id} className="list-group-item">
-                                <div className="d-flex justify-content-between">
-                                    <p>{movie.title}</p>
-                                    <p>{movie.vote_average}</p>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
 
-        </div>)
+            </div>)
     }
-
-
 };
 
